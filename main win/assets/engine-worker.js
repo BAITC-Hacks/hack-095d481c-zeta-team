5b0d6cbcb780c8c40d8c069b3736234e66e711d1
maksattut <maksat_tut@tutamail.com>
2026-09-23 16:57:51 +0500

    Delete main win directory

--- main win/assets/engine-worker.js
+++
@@ -1,1 +0,0 @@
-﻿'use strict';importScripts('engine.js','planner.js');self.onmessage=e=>{try{self.postMessage({data:PF.build(e.data.sheets)});}catch(error){self.postMessage({error:error.message});}};
